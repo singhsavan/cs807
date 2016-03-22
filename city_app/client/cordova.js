c287fda632eac787af1506c5475fd12f5a007829
singhsavan <singhsavan@gmail.com>
2016-03-22 15:06:12 -0400

    CHANGED google API KEY - enabled key
    New api key:- call to the google api not working
    error 200

---
+++ city_app/client/cordova.js
@@ -0,0 +1,10 @@
+Meteor.startup(function() {
+  navigator.geolocation.getCurrentPosition(success);
+});
+
+
+success = function(position) {
+  Session.set('location', position);
+  Meteor.call('fetchNearbyLocations', position.coords);
+  
+}
